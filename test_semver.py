2c881c052089198f68e5a7043b2702c79d25661a
mohira <shukatsudaison@gmail.com>
2018-11-03 14:11:21 +0900

    問題1完了

--- test_semver.py
+++ test_semver.py
@@ -8,7 +8,8 @@ class SemVer:
         self.patch = patch
 
     def get_notation(self) -> str:
-        return "1.4.2"
+        return str(self.major) + "." + str(self.minor) + "." + str(self.patch)
+
 
 class TestSemVer(unittest.TestCase):
     def test_major_minor_patchにそれぞれ1と4と2を与えてバージョンオブジェクトを作成(self):
@@ -16,6 +17,7 @@ class TestSemVer(unittest.TestCase):
 
     def test_major_minor_patchにそれぞれ2と30と400を与えてバージョンオブジェクトを作成(self):
         self.assertEqual("2.30.400", SemVer(2, 30, 400).get_notation())
+
 
 if __name__ == "__main__":
     unittest.main()
