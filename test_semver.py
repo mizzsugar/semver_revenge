6d10c9e6ae73511453f80b5c9120c6a79dda8c59
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-11-03 15:07:26 +0900

    問題2 等価性の比較を実装

--- test_semver.py
+++ test_semver.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+# from __future__ import annotations
 
 import unittest
 
@@ -12,8 +12,9 @@ class SemVer:
     def get_notation(self) -> str:
         return str(self.major) + "." + str(self.minor) + "." + str(self.patch)
 
-    def __eq__(self, other: SemVer) -> bool:
-        return True
+    def __eq__(self, other) -> bool:
+        return self.get_notation() == other.get_notation() \
+               and isinstance(other, SemVer)
 
 class TestSemVer(unittest.TestCase):
     def test_major_minor_patchにそれぞれ1と4と2を与えてバージョンオブジェクトを作成(self):
@@ -25,9 +26,13 @@ class TestSemVer(unittest.TestCase):
     def test_同じバージョンを持つ2つのオブジェクトが等しいことを確認(self):
         self.assertTrue(SemVer(1, 4, 2) == SemVer(1, 4, 2))
 
-    def test_違うバージョンを持つ2つのオブジェクトは等しくない(self):
+    def test_違うバージョンを持つ2つのオブジェクトは等しくないことを確認(self):
         self.assertFalse(SemVer(1, 4, 2) == SemVer(2, 30, 400))
 
+    def test_isinstanceの勉強(self): # 学習テスト
+        self.assertFalse(isinstance('hoge', SemVer))
+        self.assertTrue(isinstance('hoge', str))
+        self.assertTrue(isinstance(SemVer(1, 4, 2), SemVer))
 
 if __name__ == "__main__":
     unittest.main()
