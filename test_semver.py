f006a9b91f02416dda3b94235b4714bae82b2dfa
mohira <shukatsudaison@gmail.com>
2018-11-03 14:35:47 +0900

    問題2 バージョンオブジェクトが等しくないことを確認するテストを追加

--- test_semver.py
+++ test_semver.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import unittest
 
 
@@ -10,6 +12,8 @@ class SemVer:
     def get_notation(self) -> str:
         return str(self.major) + "." + str(self.minor) + "." + str(self.patch)
 
+    def __eq__(self, other: SemVer) -> bool:
+        return True
 
 class TestSemVer(unittest.TestCase):
     def test_major_minor_patchにそれぞれ1と4と2を与えてバージョンオブジェクトを作成(self):
@@ -20,6 +24,10 @@ class TestSemVer(unittest.TestCase):
 
     def test_同じバージョンを持つ2つのオブジェクトが等しいことを確認(self):
         self.assertTrue(SemVer(1, 4, 2) == SemVer(1, 4, 2))
+
+    def test_違うバージョンを持つ2つのオブジェクトは等しくない(self):
+        self.assertFalse(SemVer(1, 4, 2) == SemVer(2, 30, 400))
+
 
 if __name__ == "__main__":
     unittest.main()
